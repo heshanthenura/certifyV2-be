e38322363d54b5be2977cf8b06e4c991e6463edb
Heshan Thenura Kariyawasam <heshanthenura@protonmail.com>
2026-04-03 14:15:09 +0530

    chore: allow CORS

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import config
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import admin, certificate
 
 app = FastAPI()
+
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=["*"],
+	allow_credentials=False,
+	allow_methods=["*"],
+	allow_headers=["*"],
+)
 
 app.include_router(admin.router, prefix="/api")
 app.include_router(certificate.router, prefix="/api")
